2783448a4ad7f38afe15f8ed40d9ac29a3146403
Lidya Gebretsadik <lidyagebretsadikw@gmail.com>
2024-03-08 14:36:22 +0300

    Lab-1-variabels-and-types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = "Lidya";
+  var age = "21";
+  var favoriteColor = "Blue";
+
+  print(
+      'Hi my name is $name and i am $age years old. My favorite color is $favoriteColor ');
+}
